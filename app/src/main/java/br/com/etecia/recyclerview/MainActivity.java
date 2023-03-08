9234778af58a150f89fdcb80090be706f2bf0d5c
DaniloHY <nilo.11.dhy@gmail.com>
2023-03-07 21:25:18 -0300

    Filmes

--- app/src/main/java/br/com/etecia/recyclerview/MainActivity.java
+++ app/src/main/java/br/com/etecia/recyclerview/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Filmes> lstFilmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lstFilmes = new ArrayList<>();
+
+        lstFilmes.add(new Filmes("Beast", "Caçando um leão a solta", "Drama, Suspense, Aventura e Ação", R.drawable.beast));
     }
 }
